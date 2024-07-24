742dc7f0e8925521b387e1dce32c7d9dd6b7121e
Harsh Raj <harsh30032003@gmail.com>
2024-07-24 13:21:58 +0530

    fixed refresh token validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -167,7 +167,7 @@ const logOutUser = asyncHandler(async (req, res) => {
 
 const refreshAccessToken = (asyncHandler(async (req, res) => {
     const incomingRefreshToken = req.cookie.refreshToken || req.body.refreshToken
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
         throw new ApiError(401, "unauthorized request")
     }
     try {
